9431fe8dd2b1026a80bc976adc3bf05b08b6e7c7
Kafilat Abdulwahab <hadeneekeh01@gmail.com>
2019-03-10 13:58:17 +0100

    Have the sign-up and sign-in

---
+++ UI/script.js
@@ -0,0 +1,11 @@
+function openSignUp() {
+    document.getElementById('sign-up').style.display = 'block';
+  }
+
+  function openSignIn() {
+    document.getElementById('sign-in').style.display = 'block';
+  }
+
+  function closeModal() {
+    document.getElementById('sign-in').style.display = 'none';
+  }
