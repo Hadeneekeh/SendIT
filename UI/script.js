8d64461ab21f1fd5bd6fb26763596f5a59d77f43
Kafilat Abdulwahab <hadeneekeh01@gmail.com>
2019-03-10 21:53:38 +0100

    The sign-up and sign-in modal

--- UI/script.js
+++ UI/script.js
@@ -1,3 +1,8 @@
+// let openSignUpModal = document.getElementById('signUpModal');
+// let openSignInModal = document.getElementById('signInModal');
+  let signUpModal = document.getElementById('closeSignUpModal');
+  let signInModal = document.getElementById('closeSignInModal');
+
 function openSignUp() {
     document.getElementById('sign-up').style.display = 'block';
   }
@@ -6,6 +11,21 @@ function openSignUp() {
     document.getElementById('sign-in').style.display = 'block';
   }
 
-  function closeModal() {
+  function closeSignInModal() {
     document.getElementById('sign-in').style.display = 'none';
+  }
+
+  function closeSignUpModal() {
+    document.getElementById('sign-up').style.display = 'none';
+  }
+
+//   openSignUpModal.onclick = openSignUp();
+
+//   openSignInModal.onclick = openSignIn();
+
+    signUpModal.onclick = function() {
+      closeSignUpModal();
+  }
+    signInModal.onclick = function () {
+      closeSignInModal();
   }
